391a5befdfa5688bee10465ecb14a5b70119ecf9
Mahesh Vaidya <mahesh.vaidya@awanipro.com>
2025-04-03 20:50:04 +0530

    fix ALB Certificate arn

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -63,7 +63,7 @@ resource "aws_lb_listener" "https" {
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = var.certificate_arn
+  certificate_arn   = var.alb_certificate_arn
 
   default_action {
     type             = "forward"
