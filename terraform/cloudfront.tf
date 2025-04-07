fad87fcb6b6cfc433d7d193366122051adf6b8bf
Mahesh Vaidya <mahesh.vaidya@tpv-tech.com>
2025-04-07 15:01:57 +0530

    fix

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -35,7 +35,6 @@ resource "aws_cloudfront_distribution" "app" {
       }
     }
 
-    viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
     max_ttl                = 86400
