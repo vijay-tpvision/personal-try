85b890fe56c613238570b373321583199ce404ee
MAHESH VAIDYA <forvaidya@gmail.com>
2025-04-10 12:09:02 +0530

    remove flow logs

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,3 @@
-
 # VPC
 resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/22"
@@ -22,10 +21,6 @@ resource "aws_internet_gateway" "main" {
     environment = "denzopa-dev"
   }
 }
-
-
-
-
 
 # Public Subnets
 resource "aws_subnet" "public_1" {
@@ -159,70 +154,4 @@ resource "aws_route_table_association" "private_2" {
 resource "aws_route_table_association" "private_3" {
   subnet_id      = aws_subnet.private_3.id
   route_table_id = aws_route_table.private.id
-}
-
-# VPC Flow Logs
-resource "aws_flow_log" "vpc" {
-  iam_role_arn    = aws_iam_role.vpc_flow_logs.arn
-  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
-  traffic_type    = "ALL"
-  vpc_id          = aws_vpc.main.id
-}
-
-resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
-  name              = "/aws/vpc-flow-logs/denzopa"
-  retention_in_days = 30
-
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    Name        = "denzopa-vpc-flow-logs"
-    project     = "denzopa"
-    environment = "denzopa-dev"
-  }
-}
-
-resource "aws_iam_role" "vpc_flow_logs" {
-  name = "denzopa-vpc-flow-logs-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "vpc-flow-logs.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = {
-    project     = "denzopa"
-    environment = "denzopa-dev"
-  }
-}
-
-resource "aws_iam_role_policy" "vpc_flow_logs" {
-  name = "denzopa-vpc-flow-logs-policy"
-  role = aws_iam_role.vpc_flow_logs.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "logs:DescribeLogGroups",
-          "logs:DescribeLogStreams"
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      }
-    ]
-  })
 } 
