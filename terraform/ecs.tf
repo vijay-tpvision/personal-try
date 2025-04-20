d2ec659301f000648272ec63963a4709250fdc0f
MAHESH VAIDYA <forvaidya@gmail.com>
2025-04-20 11:12:09 +0530

    fix

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -152,14 +152,6 @@ resource "aws_ecs_task_definition" "app" {
           awslogs-stream-prefix = "ecs"
         }
       }
-
-      healthCheck = {
-        command     = ["CMD-SHELL", "curl -f http://nuxt-app-container:3000 || exit 1"]
-        interval    = 30
-        timeout     = 5
-        retries     = 3
-        startPeriod = 60
-      }
     },
     {
       name      = "healthcheck"
